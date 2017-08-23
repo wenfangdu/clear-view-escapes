945bc67522f048eda8f931302ff9916eaafc46cf
Leon <849995097@qq.com>
2017-08-23 21:09:59 +1200

    completed sprite gulp task

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -20,3 +20,19 @@ let config = {
     }
   }
 }
+gulp.task("clean", function() {
+  return del("src/assets/img/sprite")
+})
+gulp.task("createSprite", ["clean"], function() {
+  return gulp
+    .src("src/assets/img/icons/**/*.svg")
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest("src/assets/img/sprite"))
+})
+gulp.task("copyCss", ["createSprite"], function() {
+  return gulp
+    .src("src/assets/img/sprite/css/sprite.css")
+    .pipe(rename("_sprite.css"))
+    .pipe(gulp.dest("src/dev/css/modules"))
+})
+gulp.task("sprite", ["clean", "createSprite", "copyCss"])
